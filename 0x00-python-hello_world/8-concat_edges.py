1ea194267c089b67c7897932679263c252dc8f4b
lanlokun <mlanlokun@gmail.com>
2022-11-30 03:45:01 -0800

    updates

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-
-result_str = str[39:67] + str[107:112] + str[:6]
+result_str = str[39:67] + str[107:112] + str[0:6]
 print(result_str)
