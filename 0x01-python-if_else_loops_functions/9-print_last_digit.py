0d126e28d69f326e6bda8a6c01ce4421dcb1b0c1
lanlokun <mlanlokun@gmail.com>
2022-12-03 21:36:36 +0000

    print last digit error fix

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,9 +2,12 @@
 
 #Print the last digit of a number
 def print_last_digit(number):
+    exe = 0
     if number < 0:
-        number = ((-1) * number) % 10
-    else:
-        number = number % 10
-
-    print("{:d}\n".format(number), end="")
+        number *= -1
+    exe = 1
+    last = number % 10
+    if exe == 1:
+        number *= -1
+    print("{:d}".format(last), end="")
+    return last
