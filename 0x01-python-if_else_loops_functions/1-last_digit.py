8541639159ec58f4c687fedf4556f460ac91eeb0
lanlokun <mlanlokun@gmail.com>
2022-12-03 20:24:46 +0000

    last digit updates

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -6,6 +6,8 @@ if number < 0:
     last_digit = number % -10 
 if number > 0:
     last_digit = number % 10
+if number == 0:
+    last_digit = 0
 if last_digit > 5:
     print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
 elif last_digit == 0:
