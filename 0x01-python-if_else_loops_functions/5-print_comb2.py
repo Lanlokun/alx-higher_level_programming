711045bf6ee4caffecfce3ac9216a51ece54ff33
lanlokun <mlanlokun@gmail.com>
2022-12-03 20:53:02 +0000

    updates

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -5,7 +5,10 @@
 i = 0
 
 while i < 100:
-    if i == 99:
-        print("{:02d}".format(i))
-    print("{:02d}, ".format(i), end="")
+    
+    if i != 99:
+        print("{:02d}, ".format(i), end="")
+    else:
+        print("{:02d}".format(i), end=" \n")
+    
     i += 1
