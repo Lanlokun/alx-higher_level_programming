29bd2c95f2cb53a1850c500180bf044233a6516a
lanlokun <mlanlokun@gmail.com>
2022-12-03 21:30:16 +0000

    print last digit

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+#Print the last digit of a number
+
+def print_last_digit(number):
+    if number < 0:
+        last_digit = number % -10 
+    if number > 0:
+        last_digit = number % 10
+    if number == 0:
+        last_digit = 0
+    print("{:d}".format(last_digit), end="")
+    print()
