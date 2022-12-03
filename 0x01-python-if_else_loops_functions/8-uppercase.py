f890ea288254b176bc67421465f6889c8f230247
lanlokun <mlanlokun@gmail.com>
2022-12-03 21:13:17 +0000

    covert to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+# function that prints a string in uppercase followed by a new line.
+
+def uppercase(str):
+
+    for i in str:
+        if ord(i) >= 97 and ord(i) <= 122:
+            print("{}".format(chr(ord(i) - 32)), end="")
+        else:
+            print("{}".format(i), end="")
+    print("")
