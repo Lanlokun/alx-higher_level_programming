c3826c6fc3feb22d62b5834761d15481ca483a1b
lanlokun <mlanlokun@gmail.com>
2022-12-05 12:04:13 +0000

    alphabets in reverse updates

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-""""
-Prints the alphabet in reverse order, 
+""""Prints the alphabet in reverse order, 
 alternating lowercase and uppercase letters,
 not followed by a new line
 """
