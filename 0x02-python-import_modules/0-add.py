76b01b70d52ebac710a318c47c54af18f09c66b2
lanlokun <mlanlokun@gmail.com>
2022-12-06 11:21:24 +0000

    import simple module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-from add_0 import add
+if __name__ == "__main__":
+    from add_0 import add
 
-a = 1
-b = 2
+    """
+    importing add_0 module
 
+    args: a and b
 
+    return: a + b
 
-print(f"{a} + {b} =  {add(a, b)}")
+    """
+
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
 
