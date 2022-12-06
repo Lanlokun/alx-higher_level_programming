4784715ff77a9032de866e9e0b37075b84c838ea
lanlokun <mlanlokun@gmail.com>
2022-12-06 11:33:38 +0000

    import calculator module

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import *
+
+    """
+    importing calculator_1 module
+    
+    args: a and b
+    
+    return: a + b, a - b, a * b, a / b
+
+  
+    """
+
+    a = 10
+    b = 5
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+
+
+
