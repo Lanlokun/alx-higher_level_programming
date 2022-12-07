78fb9af6ceb9a8cae7f000420da401cc0521b074
lanlokun <mlanlokun@gmail.com>
2022-12-07 18:13:32 +0000

    updates to add module

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
+
 if __name__ == "__main__":
     from add_0 import add
-
-    """
-    importing add_0 module
-
-    args: a and b
-
-    return: a + b
-
-    """
-
+    
     a = 1
     b = 2
-
     print(f"{a} + {b} = {add(a, b)}")
 
