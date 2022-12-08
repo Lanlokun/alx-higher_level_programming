7621c153a9323a31b5197df7f85fb5c789aedade
lanlokun <mlanlokun@gmail.com>
2022-12-08 12:14:23 +0000

    replace element in list updates

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    for i in range(len(my_list)):
-        if i == idx:
-            my_list[i] = element
-            return my_list
+    if 0 <= idx < len(my_list):
+        my_list[idx] = element
+    return(my_list)
+        
+        
