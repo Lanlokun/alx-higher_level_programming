086ad39c1e4f8f6fcead5590c7944d1eee09087a
lanlokun <mlanlokun@gmail.com>
2022-12-09 12:38:06 +0000

    search & replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""
+search and replace in a list
+
+"""
 def search_replace(my_list, search, replace):
     my_list = list(map(lambda x: replace if x == search else x, my_list))
     return (my_list)
