09d8fd947ce57faca39d16b71d2d199ff48eba93
malik <mlanlokun@gmail.com>
2023-02-16 15:13:04 +0200

    change column name

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
     -- script that lists all records of the table second_table ordered by score
-SELECT score, Name FROM second_table ORDER BY score DESC;
+SELECT score, cname FROM second_table ORDER BY score DESC;
