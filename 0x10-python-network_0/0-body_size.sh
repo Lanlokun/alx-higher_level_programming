db6a2768f9e47775e2e3199f44274279e6a85eea
malik <mlanlokun@gmail.com>
2023-04-02 19:01:11 +0200

    3 lines long

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-
-# get the url from args
-
 url="$1"
-
-# send http request using curl
-
-response=$(curl -sI "$url")
-
-# display the size of the body
-
-echo "$response" | grep "Content-Length" | cut -d " " -f 2
+curl -sI $url | grep "Content-Length" | cut -d " " -f 2
