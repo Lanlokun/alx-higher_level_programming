67e514882739a3a5bdd611f4d616b4518c4400a0
malik <mlanlokun@gmail.com>
2023-04-06 03:21:35 +0200

    refactor code

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,15 +2,14 @@
 
 """ Python script that takes in a URL, sends a request to the URL and displays the body of the response (decoded in utf-8). """
 
-import urllib.request
-import urllib.error
+from urllib import request, error
 import sys
 
 if __name__ == "__main__":
     try:
-        with urllib.request.urlopen(sys.argv[1]) as response:
+        with request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
-    except urllib.error.HTTPError as e:
+    except error.HTTPError as e:
         print("Error code: {}".format(e.code))
     
     
