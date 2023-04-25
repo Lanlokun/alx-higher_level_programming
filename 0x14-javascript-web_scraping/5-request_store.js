4646abff93d308c5a604cc973e1a4d7fb1ec9059
malik <mlanlokun@gmail.com>
2023-04-25 21:02:20 +0200

    Loripsum

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+// Script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+
+request(url, function (err, response, body)
+{
+    if (err)
+    {
+        console.log(err);
+    }
+    else
+    {
+        fs.writeFile(filePath, body, 'utf8', function (err, data)
+        {
+            if (err) 
+            { 
+                console.log(err);
+            } 
+        }
+        );
+    }
+}
+);
